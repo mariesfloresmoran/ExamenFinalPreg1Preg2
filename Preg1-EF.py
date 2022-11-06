9e3a00c9e75e0c0903dfff8d98e83711235d9ea1
mariesfloresmoran <mariesfloresmoran@gmail.com>
2022-11-06 11:58:48 -0500

    preg1 opc 5 y 6 y agregué menú principal.

--- Preg1-EF.py
+++ Preg1-EF.py
@@ -89,11 +89,55 @@ def eliminarlibro():
 
     print("La nueva lista de Libros es: ")
     for l in libros:
-        print(l)
+        print(l)
 
 # Opción 5: Buscar libro por ISBN o por título.
 
+def menu_busqueda_isbn_titulo():
+    global libros
+    def buscar_por_isbn():
+        isbn = input ("Ingrese la ISBN: ")
+        resultado_busqueda_isbn = [i for i in libros if isbn.lower() in i.ISBN.lower()]
+        if resultado_busqueda_isbn:
+            print("Libros encontrados:")
+            for libro in resultado_busqueda_isbn:
+                print(libro.mostrar_datos())
+                break
+        else:
+            print ("No se encontraron coincidencias, intente con otra búsqueda.")
+
+    def buscar_por_titulo():
+        titulo = input ("Ingrese el Título: ")
+        resultado_busqueda_titulo = [i for i in libros if titulo.lower() in i.titulo.lower()]
+        if resultado_busqueda_titulo:
+            print("Libros encontrados:")
+            for libro in resultado_busqueda_titulo:
+                print(libro.mostrar_datos())
+        else:
+            print ("No se encontraron coincidencias, intente con otra búsqueda.")
+
+    while True:
+        print("Elija la opción por la cual desee buscar:")
+        print("1. Buscar por ISBN")
+        print("2. Buscar por Título")
+        print("3. Para salir")
+        
+        opcion = int(input("Ocpción: "))
+        if opcion == 1:
+            buscar_por_isbn()
+        elif opcion == 2:
+            buscar_por_titulo()
+        elif opcion == 3:
+            break
+
 # Opción 6: Ordenar libros por título.
+
+def ordenar_libro_por_titulo():
+    print("La lista sin ordenar era: ")
+    mostrar_libros()
+    libros.sort(key=lambda libro: libro.titulo)
+    print("La nueva lista ordenada es: ")
+    mostrar_libros()
 
 # Opción 7: Buscar libros por autor, editorial o género. 
 
@@ -104,3 +148,40 @@ def eliminarlibro():
 # Opción 10: Guardar libros en archivo de disco duro (.txt o csv).
 
 #MENU PRINCIPAL
+def mainmenu():
+    while True:
+        print("\n")
+        print("            Bienvenidos            ")
+        print("               Menú                ")
+        print("\n")
+        print("Seleccione una de las siguientes opciones: ");
+        print("1. Leer archivo de disco duro")
+        print("2. Listar libros")
+        print("3. Agregar libro")
+        print("4. Eliminar libro")
+        print("5. Buscar libro por ISBN o Título")
+        print("6. Ordenar libros por título")
+        print("7. Buscar libro por autor, editorial o género")
+        print("8. Buscar libros por número de autores")
+        print("9. Editar o actualizar datos de un libro")
+
+        opcion = int(input("Ocpción: "))
+        if opcion == 1:
+            pass
+        elif opcion == 2:
+            listadoLibros()
+        elif opcion == 3:
+            registrarnuevolibro()  
+        elif opcion == 4:
+            eliminarlibro() 
+        elif opcion == 5:
+            menu_busqueda_isbn_titulo()
+        elif opcion == 6:
+            ordenar_libro_por_titulo()
+        elif opcion == 7:
+            menu_busqueda_autor_editorial_genero()
+        elif opcion == 8:
+            menu_busqueda_cant_autores()  
+        elif opcion == 9:
+            editaractualizarlibro()
+mainmenu() 
